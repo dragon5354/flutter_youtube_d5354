147fda2a8f06c31ef72469743054dc374b20ff31
dragon5354 <dragon5354@naver.com>
2023-11-09 13:53:52 +0900

    기본 설정, 사용할 플러그인, asset 추가

---
+++ lib/src/app.dart
@@ -0,0 +1,13 @@
+// 시작시 최초로 보여주는 페이지
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
