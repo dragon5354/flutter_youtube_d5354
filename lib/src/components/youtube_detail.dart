99755498e2cf7ac9f78ba6258c3d2394c4aeab51
dragon5354 <dragon5354@naver.com>
2023-11-10 10:47:01 +0900

    detail 페이지 view

--- lib/src/components/youtube_detail.dart
+++ lib/src/components/youtube_detail.dart
@@ -1,14 +1,157 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 class YoutubeDetail extends StatelessWidget {
   const YoutubeDetail({super.key});
 
+  Widget _titleZone() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            "유튜브 영상 다시보기",
+            style: TextStyle(fontSize: 15),
+          ),
+          Row(
+            children: [
+              Text(
+                "동영상올린사람",
+                style: TextStyle(
+                    fontSize: 12, color: Colors.black.withOpacity(0.8)),
+              ),
+              Text(" . "),
+              Text(
+                "조회수 1000회",
+                style: TextStyle(
+                    fontSize: 12, color: Colors.black.withOpacity(0.8)),
+              ),
+              Text(" . "),
+              Text(
+                "2023-11-09",
+                style: TextStyle(
+                    fontSize: 12, color: Colors.black.withOpacity(0.8)),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buttonOne(String iconPath, String text) {
+    return Column(
+      children: [
+        SvgPicture.asset("assets/svg/icons/$iconPath.svg"),
+        Text(text),
+      ],
+    );
+  }
+
+  Widget _buttonZone() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        _buttonOne("like", "1000"),
+        _buttonOne("dislike", "0"),
+        _buttonOne("share", "공유"),
+        _buttonOne("save", "저장"),
+      ],
+    );
+  }
+
+  Widget _descriptionZone() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+      child: Text(
+        "안녕하세요 반갑습니다",
+        style: TextStyle(fontSize: 14),
+      ),
+    );
+  }
+
+  Widget get line => Container(
+        height: 1,
+        color: Colors.black.withOpacity(0.1),
+      );
+
+  Widget _ownerZone() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: Colors.grey.withOpacity(0.5),
+            backgroundImage: Image.network(
+                    "https://icon-library.com/images/guest-icon-png/guest-icon-png-6.jpg")
+                .image,
+          ),
+          SizedBox(width: 15,),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  "동영상주인",
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  "구독자 10000",
+                  style: TextStyle(
+                      fontSize: 14, color: Colors.black.withOpacity(0.6)),
+                )
+              ],
+            ),
+          ),
+          GestureDetector(
+            child: Text(
+              "구독",
+              style: TextStyle(color: Colors.red, fontSize: 16),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  // 동영상 아래 내용 들어갈 부분
+  Widget _description() {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          _titleZone(),
+          line,
+          _descriptionZone(),
+          _buttonZone(),
+          SizedBox(
+            height: 20,
+          ),
+          line,
+          _ownerZone(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(Get.parameters["videoId"]!),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Container(
+            height: 250,
+            color: Colors.grey.withOpacity(0.5),
+          ),
+          Expanded(
+            child: _description(),
+          )
+        ],
       ),
     );
   }
